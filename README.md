b698100ad5d04f074ef733ec2912add938680505
danylaksono <danylaksono@ugm.ac.id>
2024-03-24 22:48:30 +0000

    add basemap

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Serverless WebMap Prototype
 
 A prototype of a serverless web map app based on PMTiles, using Maplibre GL JS and Cloudflare Workers.
+Heavily inspired by this [Simon Willison’s TILs](https://github.com/simonw/til/blob/main/gis/pmtiles.md).
+
+## Components
+
+- **PMTiles**: A specification for serving raster tiles from a key-value store.
+- **Maplibre GL JS**: A JavaScript library for interactive, customizable vector maps.
+- **Cloudflare Workers**: A serverless platform for deploying and running code at the edge of the network.
--- main.js
+++ main.js
@@ -1,82 +1,48 @@
 import "./style.css";
-import "./style.css";
 import * as pmtiles from "pmtiles";
 import * as maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 // import layers from "protomaps-themes-base";
+// import style from "./components/style";
 
 const protocol = new pmtiles.Protocol();
 maplibregl.addProtocol("pmtiles", protocol.tile);
 
-const style = {
-  version: 8,
-  glyphs: "https://cdn.protomaps.com/fonts/pbf/{fontstack}/{range}.pbf",
-  sources: {
-    osm: {
-      type: "raster",
-      tiles: ["https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"],
-      tileSize: 256,
-      attribution: "&copy; OpenStreetMap Contributors",
-      maxzoom: 19,
-    },
-    protomapsRaster: {
-      type: "raster",
-      url: "pmtiles://https://pub-ee5ad887928649d582242a69272f342c.r2.dev/jogja_1925.pmtiles",
-      // url: `pmtiles://${location.protocol}//${location.host}${location.pathname}jogja_1925.pmtiles`,
-    },
-    protomapsVector: {
-      type: "vector",
-      url: "pmtiles://https://pub-ee5ad887928649d582242a69272f342c.r2.dev/yogyakarta.pmtiles",
-      attribution:
-        '<a href="https://protomaps.com">Protomaps</a> © <a href="https://openstreetmap.org">OpenStreetMap</a>',
-    },
-  },
-  layers:
-    // layers("protomaps", "light") // if protomaps-themes-base is used
-    [
-      // {
-      //   id: "osm",
-      //   type: "raster",
-      //   source: "osm",
-      // },
-      {
-        id: "Jogja1925",
-        type: "raster",
-        source: "protomapsRaster",
-        opacity: 0.65,
-        minzoom: 0,
-        maxzoom: 20,
-      },
-
-      // { // this demonstrates how to add a vector layer
-      //   id: "buildings",
-      //   type: "fill",
-      //   source: "protomapsVector",
-      //   "source-layer": "buildings",
-      //opacity: 0.65,
-      //   paint: {
-      //     "fill-color": "steelblue",
-      //   },
-      // },
-    ],
-};
-
 // Initialise the map
 const map = new maplibregl.Map({
   container: "map",
-  style: style,
+  // style: style,
+  style: "https://tiles.stadiamaps.com/styles/osm_bright.json",
   zoom: 12,
   center: [110.365685, -7.794314],
+});
+
+map.once("load", () => {
+  map.addSource("protomapsRaster", {
+    type: "raster",
+    url: "pmtiles://https://pub-ee5ad887928649d582242a69272f342c.r2.dev/jogja_1925.pmtiles",
+  });
+
+  map.addLayer({
+    id: "Jogja1925",
+    type: "raster",
+    source: "protomapsRaster",
+    opacity: 0.65,
+    minzoom: 0,
+    maxzoom: 20,
+  });
 });
 
 const slider = document.getElementById("slider");
 const sliderValue = document.getElementById("slider-value");
 
 map.on("load", () => {
-  const myBounds = [109.404327, -8.42347, 111.490357, -7.43056]; //myMap.getSource("protomaps").bounds;
+  const myBounds = [109.404327, -8.42347, 111.490357, -7.43056]; // from bboxfinder.com
+  //other: myMap.getSource("protomaps").bounds;
   map.setMaxBounds(myBounds);
 
   slider.addEventListener("input", (e) => {
+    // alternatively, https://github.com/mug-jp/maplibre-gl-opacity can be used
     map.setPaintProperty(
       "Jogja1925",
       "raster-opacity",
